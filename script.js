3135fd70c04ec41fe1510c7069a3a017e206921b
Michal Škoula <imichalthemichal@gmail.com>
2023-07-18 09:34:08 +0200

    Minor changes, organized css

---
+++ script.js
@@ -0,0 +1,22 @@
+console.log('working')
+
+function copyServerAdress() {
+  const ipButton = document.querySelector('.ip-adress')
+  const copyText = document.getElementById('js-copy')
+  console.log(copyText.value)
+  copyText.select()
+
+  navigator.clipboard.writeText(ipButton)
+  console.log(ipButton)
+}
+
+function openNavbar() {
+  const navbar = document.querySelector('.navbar')
+  // console.log(navbar)
+  
+  if(navbar.className === 'navbar') {
+    navbar.className += ' reactive'
+  } else {
+    navbar.className = 'navbar'
+  }
+}
