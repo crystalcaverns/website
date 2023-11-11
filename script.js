85358a87b5c50092a325f8047c698fed96c0f8d5
Michal <imichalthemichal@gmail.com>
2023-11-11 23:04:05 +0100

    v2 first commit, remove all css

--- script.js
+++ script.js
@@ -1,15 +1,3 @@
-console.log('Welcome to Crystal Caverns! :D')
-// function copyServerAdress() {
-//   let ipButton = document.querySelector('.ip-adress')
-//   ipButton = document.createElement('button');
-//   ipButton.classList.add('ip-adress');
-//   ipButton.textContent = '192.168.1.1';
-
-//   ipButton.addEventListener('click', () => {
-//     const textToCopy = ipButton.textContent;
-//     let clipboard = new ClipboardJS();
-//     clipboard.writeText(textToCopy);
-// });
 function openNavbar() {
   const navbar = document.querySelector('.navbar')
   
@@ -19,13 +7,3 @@ function openNavbar() {
     navbar.className = 'navbar'
   }
 }
-// function openTrailer() {
-//   popupWindow = document.querySelector('iframe');
-//   if(popupWindow.className === '') {
-//     popupWindow.className = 'trailer-open'
-//   } else {
-//     popupWindow.className = ' '
-//   }
-//   console.log(popupWindow)
-
-// }
